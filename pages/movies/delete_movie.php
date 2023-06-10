858ff63e59d51b975021aa1e50f468d46f3dfc8d
Mateo Razquin <77582478+razquinmateo@users.noreply.github.com>
2023-06-10 20:31:20 -0300

    Update delete_movie.php

--- pages/movies/delete_movie.php
+++ pages/movies/delete_movie.php
@@ -11,24 +11,19 @@ if(isset($_POST['movie-id']) && $_POST['movie-id'] != "") {
     $idPelicula = $_POST['movie-id'];
 
     $delete_query_pagos = "DELETE FROM pagos WHERE idPeliculaAlquilo IN (SELECT idPelicula FROM alquileres WHERE idPelicula = $idPelicula)";
-    $query_result_pagos = mysqli_query($connection, $delete_query_pagos);
-
     $delete_query_alquileres = "DELETE FROM alquileres WHERE idPelicula = $idPelicula";
-    $query_result_alquileres = mysqli_query($connection, $delete_query_alquileres);
-
     $delete_query_actoresDePeliculas = "DELETE FROM actoresDePeliculas WHERE idPelicula = $idPelicula";
-    $query_result_actoresDePeliculas = mysqli_query($connection, $delete_query_actoresDePeliculas);
-
     $delete_query_idiomasDePeliculas = "DELETE FROM idiomasDePeliculas WHERE idPelicula = $idPelicula";
-    $query_result_idiomasDePeliculas = mysqli_query($connection, $delete_query_idiomasDePeliculas);
-
     $delete_query_categoriasDePeliculas = "DELETE FROM categoriasDePeliculas WHERE idPelicula = $idPelicula";
-    $query_result_categoriasDePeliculas = mysqli_query($connection, $delete_query_categoriasDePeliculas);
-
     $delete_query_inventario = "DELETE FROM inventario WHERE idPelicula = $idPelicula";
-    $query_result_inventario = mysqli_query($connection, $delete_query_inventario);
-
     $delete_query_peliculas = "DELETE FROM peliculas WHERE idPelicula = $idPelicula";
+    
+    $query_result_pagos = mysqli_query($connection, $delete_query_pagos);
+    $query_result_alquileres = mysqli_query($connection, $delete_query_alquileres);
+    $query_result_actoresDePeliculas = mysqli_query($connection, $delete_query_actoresDePeliculas);
+    $query_result_idiomasDePeliculas = mysqli_query($connection, $delete_query_idiomasDePeliculas);
+    $query_result_categoriasDePeliculas = mysqli_query($connection, $delete_query_categoriasDePeliculas);
+    $query_result_inventario = mysqli_query($connection, $delete_query_inventario);
     $query_result_peliculas = mysqli_query($connection, $delete_query_peliculas);
 
     if($query_result_pagos && $query_result_alquileres && $query_result_actoresDePeliculas && $query_result_idiomasDePeliculas && $query_result_categoriasDePeliculas && $query_result_inventario && $query_result_peliculas) {

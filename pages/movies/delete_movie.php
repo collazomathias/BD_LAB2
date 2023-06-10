238f419f1a0f6f83af19fe8c58c2d4d34fbd771c
Mateo Razquin <77582478+razquinmateo@users.noreply.github.com>
2023-06-10 03:09:41 -0300

    Update delete_movie.php

--- pages/movies/delete_movie.php
+++ pages/movies/delete_movie.php
@@ -7,7 +7,7 @@ echo isset($_POST['movie-id']) ? $_POST['movie-id'].'<br>' : '<br>';
 require '../../connection_manager.php';
 $connection = connect();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['movie-id']) && $_POST['movie-id'] != "") {
+if(isset($_POST['movie-id']) && $_POST['movie-id'] != "") {
     $idPelicula = $_POST['movie-id'];
     $delete_query_peliculas = "DELETE FROM peliculas WHERE idPelicula = $idPelicula";
     $delete_query_actoresDePeliculas = "DELETE FROM actoresDePeliculas WHERE idPelicula = $idPelicula";
@@ -16,30 +16,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['movie-id']) && $_POST[
     $delete_query_inventario = "DELETE FROM inventario WHERE idPelicula = $idPelicula";
     $delete_query_alquileres = "DELETE FROM alquileres WHERE idPelicula = $idPelicula";
     $delete_query_pagos = "DELETE FROM pagos WHERE idPeliculaAlquilo = $idPelicula";
-    
-    $query_result_peliculas = mysqli_query($connection, $delete_query_peliculas);
-    $affected_rows_peliculas = mysqli_affected_rows($connection);
 
-    if ($affected_rows_peliculas > 0) {
-        mysqli_query($connection, $delete_query_actoresDePeliculas);
-        mysqli_query($connection, $delete_query_idiomasDePeliculas);
-        mysqli_query($connection, $delete_query_categoriasDePeliculas);
-        mysqli_query($connection, $delete_query_inventario);
-        mysqli_query($connection, $delete_query_alquileres);
-        mysqli_query($connection, $delete_query_pagos);
-        
-        $affected_rows_actoresDePeliculas = mysqli_affected_rows($connection);
-        $affected_rows_idiomasDePeliculas = mysqli_affected_rows($connection);
-        $affected_rows_categoriasDePeliculas = mysqli_affected_rows($connection);
-        $affected_rows_inventario = mysqli_affected_rows($connection);
-        $affected_rows_alquileres = mysqli_affected_rows($connection);
-        $affected_rows_pagos = mysqli_affected_rows($connection);
-        
-        if ($affected_rows_actoresDePeliculas >= 0 && $affected_rows_idiomasDePeliculas >= 0 && $affected_rows_categoriasDePeliculas >= 0 && $affected_rows_inventario >= 0 && $affected_rows_alquileres >= 0 && $affected_rows_pagos >= 0) {
-            echo '<script>window.location="/?page=movies/movies&modal-success=1&action=delete&type=movie";</script>';
-        } else {
-            echo '<script>window.location="/?page=movies/movies&modal-failed=1&action=delete&type=movie";</script>';
-        }
+    $query_result_peliculas = mysqli_query($connection, $delete_query_peliculas);
+
+    if($query_result_peliculas) {
+        $query_result_actoresDePeliculas = mysqli_query($connection, $delete_query_actoresDePeliculas);
+        $query_result_idiomasDePeliculas = mysqli_query($connection, $delete_query_idiomasDePeliculas);
+        $query_result_categoriasDePeliculas = mysqli_query($connection, $delete_query_categoriasDePeliculas);
+        $query_result_inventario = mysqli_query($connection, $delete_query_inventario);
+        $query_result_alquileres = mysqli_query($connection, $delete_query_alquileres);
+        $query_result_pagos = mysqli_query($connection, $delete_query_pagos);
+
+        echo (isset($query_result_actoresDePeliculas) && isset($query_result_idiomasDePeliculas) && isset($query_result_categoriasDePeliculas) && isset($query_result_inventario) && isset($query_result_alquileres) && isset($query_result_pagos)) ? '<script>window.location="/?page=movies/movies&modal-success=1&action=delete&type=movie";</script>' : '<script>window.location="/?page=movies/movies&modal-failed=1&action=delete&type=movie";</script>';    
     } else {
         echo '<script>window.location="/?page=movies/movies&modal-failed=1&action=delete&type=movie";</script>';
     }
